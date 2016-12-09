308586b323584a16f0c059c7d0c5b3e6c541355e
Ian DeGroot <ian.degroot@yahoo.com>
2016-12-08 22:43:11 -0600

    Updating the way that the surface width is calculated.

--- snake_utils.py
+++ snake_utils.py
@@ -11,17 +11,16 @@ class MultiColorText:
 
     def __init__(self, text, font):
         self.chars = [font.render(c, True, rand_color()) for c in text]
-        self.char_w = self.chars[0].get_width()
+        self.char_ws = [c.get_width() for c in self.chars]
         self.char_h = self.chars[0].get_height()
-        self.full_text = pygame.Surface((self.char_w * len(self.chars), self.char_h))
+        self.num_small_chars = text.lower().count("l") + text.lower().count("i")
+        self.num_big_chars = text.lower().count("m") + text.lower().count("w")
+        self.full_text = pygame.Surface((sum(self.char_ws), self.char_h))
 
         # Blit each individual character onto the full_text surface, which will contain the whole word
         curr_left = 0
         for i in xrange(len(self.chars)):
-            if text[i].lower() == "i" or text[i].lower() == "l":
-                self.full_text.blit(self.chars[i], (curr_left, 0))
-                curr_left -= self.char_w / 2
-            else:
-                self.full_text.blit(self.chars[i], (curr_left, 0))
+            self.full_text.blit(self.chars[i], (curr_left, 0))
 
-            curr_left += self.char_w
+            curr_left += self.char_ws[i]
+
